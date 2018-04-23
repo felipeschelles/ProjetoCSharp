d3a713273f4bb759ff802c215e011ad37813f9f1
Guilherme <guilinfer@gmail.com>
2018-04-23 19:30:52 -0300

    Tela Extrato

--- BancoEletronico/TelaInicial/Extrato.xaml.cs
+++ BancoEletronico/TelaInicial/Extrato.xaml.cs
@@ -26,14 +26,56 @@ namespace TelaInicial
         public int tipoConta;
 
         public Extrato()
-        {            
+        {
             InitializeComponent();
+            
         }
 
         private void lblNomeExtrato_ContextMenuClosing(object sender, ContextMenuEventArgs e)
         {
             Cliente c = new Cliente();
             //lblNomeExtrato = Convert.ToString(c.Nome);
+        }
+
+        private void btnGerar_Click(object sender, RoutedEventArgs e)
+        {
+            if (tipoConta == 1)
+            {
+                ContaCController cc = new ContaCController();
+                ContaCorrente co = cc.ClienteConta(conta);
+                if (co != null)
+                {
+                    txtNome.Text = co._Cliente.Nome;
+                    txtCpf.Text = co._Cliente.Cpf;
+                    txtNumeroConta.Text = co.Numero.ToString();
+                    txtSaldo.Text = co.Saldo.ToString();
+
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Operação não realizada.");
+
+                }
+            }
+            else
+            {
+                ContaPController cp = new ContaPController();
+                ContaPoupanca cpp = cp.ClienteConta(conta);
+                if (cpp != null)
+                {
+                    txtNome.Text = cpp._Cliente.Nome;
+                    txtCpf.Text = cpp._Cliente.Cpf;
+                    txtNumeroConta.Text = cpp.Numero.ToString();
+                    txtSaldo.Text = cpp.Saldo.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Operação não realizada.");
+
+                }
+            }
         }
     }
 }
